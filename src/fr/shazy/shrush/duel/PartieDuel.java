5d419045df3c63d21942c27962298edb0bc1a612
Marin <bourdonmarin@gmail.com>
2024-01-23 16:14:46 +0100

    avancée duel

--- src/fr/shazy/shrush/duel/PartieDuel.java
+++ src/fr/shazy/shrush/duel/PartieDuel.java
@@ -1,10 +1,15 @@
 package fr.shazy.shrush.duel;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Locale;
 
 public class PartieDuel {
     private boolean started;
@@ -37,6 +42,34 @@ public class PartieDuel {
             player.sendMessage(ChatColor.GREEN + "Battez-vous !");
         }
         // On téléporte les 2 joueurs dans l'arène
+        Location arene1 = new Location(Bukkit.getWorld("world"), -295, 97, -200);
+        Location arene2 = new Location(Bukkit.getWorld("world"), -300, 97, -220);
+        playersList.get(0).teleport(arene1);
+        playersList.get(1).teleport(arene2);
+        // On clear les inventaires des joueurs
+        playersList.get(0).getInventory().clear();
+        playersList.get(1).getInventory().clear();
+        // On crée une épée en pierre
+        ItemStack sword = new ItemStack(Material.STONE_SWORD);
+        // Un arc
+        ItemStack bow = new ItemStack(Material.BOW);
+        // Des pommes en or
+        ItemStack apple = new ItemStack(Material.GOLDEN_APPLE, 10);
+        // Une rod
+        ItemStack rod = new ItemStack(Material.FISHING_ROD);
+        // Des flèches
+        // On donne tout ça aux joueurs
+        playersList.get(0).getInventory().setItem(0, sword);
+        playersList.get(1).getInventory().setItem(0, sword);
+        playersList.get(0).getInventory().setItem(1, bow);
+        playersList.get(1).getInventory().setItem(1, bow);
+        playersList.get(0).getInventory().setItem(2, apple);
+        playersList.get(1).getInventory().setItem(2, apple);
+        playersList.get(0).getInventory().setItem(3, rod);
+        playersList.get(1).getInventory().setItem(3, rod);
+
+
+        // On
 
         this.started = true;
     }
